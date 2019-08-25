ee5cdba591997894bbbeb6f3e250501ce1d1334f
Ivan <ivan@Joyns-MacBook-Pro.local>
2019-08-25 20:51:07 +0200

    deactivation du button submit apres la requete dans la page login

--- src/components/Login.js
+++ src/components/Login.js
@@ -59,6 +59,7 @@ class Login extends Component{
     }
 
     handleSubmit(){
+        this.toggleSubmit();
         fetch('https://glacial-shelf-93469.herokuapp.com/login', {
             method: 'POST',
             headers: {
