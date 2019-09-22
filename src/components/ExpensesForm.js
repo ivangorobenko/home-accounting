cea1505a9f6ac573e7cc9062386d8ac3ea72f925
ivangorobenko <49490325+ivangorobenko@users.noreply.github.com>
2019-09-22 11:33:45 +0200

    ui ajustements

--- src/components/ExpensesForm.js
+++ src/components/ExpensesForm.js
@@ -1,7 +1,5 @@
 import {Component} from "react";
 import * as React from "react";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import CustomSnackBar from "./CustomSnackBar";
@@ -17,21 +15,21 @@ import CustomAppBar from "./CustomAppBar";
 import Paper from "@material-ui/core/Paper";
 
 
-class ExpensesForm extends Component{
+class ExpensesForm extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            amount:'',
-            description:'',
-            payer:'Be',
+        this.state = {
+            amount: '',
+            description: '',
+            payer: 'Be',
             category: '',
             open: false,
             submitButtonDisabled: true,
-            snackBarMessage:'',
-            snackBarClassType:''
+            snackBarMessage: '',
+            snackBarClassType: ''
         }
-        if(!this.props.isAuthenticated) {
+        if (!this.props.isAuthenticated) {
             this.props.history.push("/login");
         }
         this.submitExpenseForm = this.submitExpenseForm.bind(this);
@@ -48,19 +46,19 @@ class ExpensesForm extends Component{
         this.validateForm();
     }
 
-    submitExpenseForm(){
+    submitExpenseForm() {
         this.toggleSubmit();
         fetch('https://glacial-shelf-93469.herokuapp.com/expenses', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
-                'Authorization' :'Basic '+btoa(this.props.login +':'+this.props.password)
+                'Authorization': 'Basic ' + btoa(this.props.login + ':' + this.props.password)
             },
-            body : JSON.stringify ({
-                "amount" : this.state.amount,
-                "description" : this.state.description,
-                "payer" : this.state.payer,
+            body: JSON.stringify({
+                "amount": this.state.amount,
+                "description": this.state.description,
+                "payer": this.state.payer,
                 "category": this.state.category
             })
         }).then(
@@ -69,7 +67,7 @@ class ExpensesForm extends Component{
             });
     }
 
-    toggleSubmit(){
+    toggleSubmit() {
         this.setState(state => ({
             submitButtonDisabled: !state.submitButtonDisabled
         }));
@@ -81,15 +79,14 @@ class ExpensesForm extends Component{
         }));
     }
 
-    handleLoginResponse(response){
+    handleLoginResponse(response) {
         if (response.ok) {
             this.setState({
                 snackBarClassType: "success",
                 snackBarMessage: "Dépense envoyée"
             });
             this.emptyForm();
-        }
-        else if(!response.ok) {
+        } else if (!response.ok) {
             this.setState({
                 snackBarClassType: "error",
                 snackBarMessage: "Une erreur est survenue"
@@ -99,92 +96,87 @@ class ExpensesForm extends Component{
     }
 
     validateForm() {
-        if (this.state.amount.length > 0 && this.state.description.length > 0)
-        {
+        if (this.state.amount.length > 0 && this.state.description.length > 0) {
             this.setState({
                 submitButtonDisabled: false
             });
         }
     }
 
-    emptyForm(){
+    emptyForm() {
         this.setState({
             amount: '',
             description: '',
             category: ''
         });
-        document.getElementById('amount').value='';
-        document.getElementById('description').value='';
-        document.getElementById('category').value='';
+        document.getElementById('amount').value = '';
+        document.getElementById('description').value = '';
+        document.getElementById('category').value = '';
     }
 
-    handleBackArrowClick(){
+    handleBackArrowClick() {
         this.props.history.push("/expensestable");
     }
 
 
     render() {
-        return <Paper className="Form">
+        return <Paper className="Paper-expense-form">
             <CustomAppBar history={this.props.history}/>
-            <Card className="Card-expenses-form">
-                <CardContent  className="Card-custom">
-                    <TextField
-                        type="number"
-                        id='amount'
-                        name="amount"
-                        className="Field-expenses-form"
-                        autoComplete="off"
-                        label="€"
-                        autoFocus
-                        onChange={this.handleChange}/> <br/>
-                    <TextField
-                        id='description'
-                        name="description"
-                        className="Field-expenses-form"
-                        label="Description"
-                        autoComplete="off"
-                        onChange={this.handleChange}/><br/>
-                    <RadioGroup aria-label="payer"
-                                id='payer'
-                                name="payer"
-                                value={this.state.payer}
-                                onChange={this.handleChange}
-                                className="Radio-button"
-                                row>
-                        <FormControlLabel value="Be" control={<Radio color="primary"/>} label="Be" selected/>
-                        <FormControlLabel value="Ivan" control={<Radio color="primary"/>} label="Ivan" />
-                    </RadioGroup>
-                    <FormControl className="Category-form-control">
-                        <InputLabel htmlFor="category">Type de dépense</InputLabel>
-                        <Select
-                            value={this.state.category}
-                            onChange={this.handleChange}
-                            inputProps={{
-                                name: 'category',
-                                id: 'category',
-                            }}>
-                            <MenuItem value={"Alimentation"}>Alimentation</MenuItem>
-                            <MenuItem value={"Sorties/Voyages"}>Sorties/Voyages</MenuItem>
-                            <MenuItem value={"Émile"}>Émile</MenuItem>
-                            <MenuItem value={"Logement"}>Logement</MenuItem>
-                            <MenuItem value={"Divers"}>Divers</MenuItem>
-                        </Select>
-                    </FormControl><br/>
-                    <Button
-                        variant="contained"
-                        className="Submit-button"
-                        onClick={this.submitExpenseForm}
-                        disabled={this.state.submitButtonDisabled}>
-                        Envoyer
-                    </Button>
-                    <CustomSnackBar
-                        open={this.state.open}
-                        toggle={this.toggleChildSnackBar}
-                        snackbarMessage = {this.state.snackBarMessage}
-                        classType={this.state.snackBarClassType}
-                    />
-                </CardContent>
-            </Card>
+            <TextField
+                type="number"
+                id='amount'
+                name="amount"
+                className="Field-expenses-form"
+                autoComplete="off"
+                label="€"
+                autoFocus
+                onChange={this.handleChange}/> <br/>
+            <TextField
+                id='description'
+                name="description"
+                className="Field-expenses-form"
+                label="Description"
+                autoComplete="off"
+                onChange={this.handleChange}/><br/>
+            <RadioGroup aria-label="payer"
+                        id='payer'
+                        name="payer"
+                        value={this.state.payer}
+                        onChange={this.handleChange}
+                        className="Radio-button"
+                        row>
+                <FormControlLabel value="Be" control={<Radio color="primary"/>} label="Be" selected/>
+                <FormControlLabel value="Ivan" control={<Radio color="primary"/>} label="Ivan"/>
+            </RadioGroup>
+            <FormControl className="Category-form-control">
+                <InputLabel htmlFor="category">Type de dépense</InputLabel>
+                <Select
+                    value={this.state.category}
+                    onChange={this.handleChange}
+                    inputProps={{
+                        name: 'category',
+                        id: 'category',
+                    }}>
+                    <MenuItem value={"Alimentation"}>Alimentation</MenuItem>
+                    <MenuItem value={"Sorties/Voyages"}>Sorties/Voyages</MenuItem>
+                    <MenuItem value={"Émile"}>Émile</MenuItem>
+                    <MenuItem value={"Logement"}>Logement</MenuItem>
+                    <MenuItem value={"Divers"}>Divers</MenuItem>
+                </Select>
+            </FormControl><br/>
+            <Button
+                variant="contained"
+                className="Submit-button"
+                onClick={this.submitExpenseForm}
+                disabled={this.state.submitButtonDisabled}>
+                Envoyer
+            </Button>
+            <CustomSnackBar
+                open={this.state.open}
+                toggle={this.toggleChildSnackBar}
+                snackbarMessage={this.state.snackBarMessage}
+                classType={this.state.snackBarClassType}
+            />
         </Paper>;
     }
 }
